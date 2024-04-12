2c3ebfa9bda6d4985d30b64fa36693e214ca7149
Nickolay Petrushin <nick.mflj@gmail.com>
2024-04-12 20:18:34 +0300

    I made more at home

--- src/main/java/ru/ylab/exception/AdditionalInformationException.java
+++
@@ -1,7 +0,0 @@
-package ru.ylab.exception;
-
-public class AdditionalInformationException extends RuntimeException{
-    public AdditionalInformationException(String message) {
-        super(message);
-    }
-}
